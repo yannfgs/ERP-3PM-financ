d00d1c677d2bb39e6ff8835e409b464b228d3f2c
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-19 17:53:20 -0300

    19122301

--- public/script.js
+++ public/script.js
@@ -1,33 +1,35 @@
-// JS 
+// JS
 
-document.getElementById('financeForm').addEventListener('submit', function (e) {
-    e.preventDefault();
+document.getElementById("financeForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const tipo = document.getElementById('tipo').value;
-    const categoria = document.getElementById('categoria').value;
-    const descricao = document.getElementById('descricao').value;
-    const valor = parseFloat(document.getElementById('valor').value).toFixed(2);
-    const vencimento = document.getElementById('vencimento').value;
+  const tipo = document.getElementById("tipo").value;
+  const categoria = document.getElementById("categoria").value;
+  const descricao = document.getElementById("descricao").value;
+  const valor = parseFloat(document.getElementById("valor").value).toFixed(2);
+  const vencimento = document.getElementById("vencimento").value;
 
-    adicionarFinanceItem(tipo, categoria, descricao, valor, vencimento);
+  adicionarFinanceItem(tipo, categoria, descricao, valor, vencimento);
 });
 
 function adicionarFinanceItem(tipo, categoria, descricao, valor, vencimento) {
-    const tabela = document.getElementById('financeTable').getElementsByTagName('tbody')[0];
-    const novaLinha = tabela.insertRow();
+  const tabela = document
+    .getElementById("financeTable")
+    .getElementsByTagName("tbody")[0];
+  const novaLinha = tabela.insertRow();
 
-    const celulaTipo = novaLinha.insertCell();
-    celulaTipo.textContent = tipo;
+  const celulaTipo = novaLinha.insertCell();
+  celulaTipo.textContent = tipo;
 
-    const celulaCategoria = novaLinha.insertCell();
-    celulaCategoria.textContent = categoria;
+  const celulaCategoria = novaLinha.insertCell();
+  celulaCategoria.textContent = categoria;
 
-    const celulaDescricao = novaLinha.insertCell();
-    celulaDescricao.textContent = descricao;
+  const celulaDescricao = novaLinha.insertCell();
+  celulaDescricao.textContent = descricao;
 
-    const celulaValor = novaLinha.insertCell();
-    celulaValor.textContent = `R$ ${valor}`;
+  const celulaValor = novaLinha.insertCell();
+  celulaValor.textContent = `R$ ${valor}`;
 
-    const celulaVencimento = novaLinha.insertCell();
-    celulaVencimento.textContent = vencimento;
+  const celulaVencimento = novaLinha.insertCell();
+  celulaVencimento.textContent = vencimento;
 }
